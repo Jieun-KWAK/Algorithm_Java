3e629af7f224a592372d90db0041f17f5065c0a3
Jieun-KWAK <jieun47631@gmail.com>
2023-07-25 08:30:12 +0900

    first freshman

--- Q003.java
+++ Q003.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
+class A{
+
+}
+
 public class Q003 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
